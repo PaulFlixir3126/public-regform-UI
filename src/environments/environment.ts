03cc7a78538310d33ba3875ca1645d400e3677a1
Paul Pinto N <paul3126@Pauls-MacBook-Pro.local>
2021-01-05 08:19:28 +0530

    updated env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 const envObject = {
-  serviceUrl: 'http://localhost:6001'
+  // serviceUrl: 'http://localhost:6001'
   // serviceUrl: 'https://infrastructuremicroservicetest.herokuapp.com/api'
+  serviceUrl : 'https://mpsc-backend-server-test.herokuapp.com'
   // 
 };
 export const environment = {
